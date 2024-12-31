6b33b73cbf3d5dec91418b3c0d73f80411a44bc7
Manas <manas18244@iiitd.ac.in>
2024-12-31 22:17:39 +0530

    Use myMusicCtrl from Preferences for Soundtrack plugin
    Although many options are specific to playerctl, but keeping
    myMusicCtrl in Preferences give ideas to users.

--- src/Plugins/Soundtrack.hs
+++ src/Plugins/Soundtrack.hs
@@ -1,4 +1,4 @@
---- This plugin tracks tracks being played through playerctl tool. And provides
+--- This plugin tracks tracks being played through myMusicCtrl. And provides
 --- a user-interface to pause, play, forward and backward tracks.
 module Plugins.Soundtrack
   ( getArtist
@@ -7,10 +7,11 @@ module Plugins.Soundtrack
   , Controller(..)
   ) where
 
+import Preferences (myMusicCtrl)
 import System.Process
 import Xmobar
 
--- This utility function is needed because playerctl returns a newline appended
+-- This utility function is needed because myMusicCtrl returns a newline appended
 -- string.
 trim :: String -> String
 trim = reverse . dropWhile (== '\n') . reverse
@@ -18,14 +19,14 @@ trim = reverse . dropWhile (== '\n') . reverse
 -- Returns the artist name.
 getArtist :: IO String
 getArtist = do
-  artist <- readProcess "playerctl" ["metadata", "artist"] ""
+  artist <- readProcess myMusicCtrl ["metadata", "artist"] ""
   return $ takeWhile (/= ',') . trim $ artist
 
 -- Returns the title track that is being played.
 getTrack :: IO String
 getTrack = do
   track <-
-    readProcess "playerctl" ["metadata", "title"] "" >>= \x -> return (trim x)
+    readProcess myMusicCtrl ["metadata", "title"] "" >>= \x -> return (trim x)
   if length track > 14
     then return $ take 14 track ++ "..."
     else return track
