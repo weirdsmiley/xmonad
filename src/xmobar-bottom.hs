2d1c7ad9a5891f9cd050be3c1c168d18b3bce25a
Manas <manas18244@iiitd.ac.in>
2025-01-20 19:17:58 +0530

    Move soundtrack, cpu, ram, disk to bottom bar
    Also edit colors. Now we can show complete information for music (title,
    album, artist) using start method. This means user can modify colors
    using --template parameter settings for Soundtrack. Album cover is still
    pending.

--- src/xmobar-bottom.hs
+++ src/xmobar-bottom.hs
@@ -13,18 +13,93 @@ config =
         , "0xProto Nerd Font Regular 10"
         ]
     , allDesktops = True
-    , position = Static {xpos = 10, ypos = 1040, width = 1900, height = 30}
+    , position = Static {xpos = 10, ypos = 1050, width = 1900, height = 30}
     , border = NoBorder
-    , lowerOnStart = False
+    , lowerOnStart = True
     , hideOnStart = False
     , pickBroadest = False
-    , persistent = False
-    , alpha = 10
+    , persistent = True
+    , alpha = 0
     , fgColor = "#f8f8f8"
     , bgColor = "#5501c3"
     , commands =
-        [Run $ Soundtrack ["-t", "<fc=lightgreen><title> <artist></fc>"] 1000]
-    , template = "} %soundtrack% {"
+        [ Run
+            $ Soundtrack
+                ["-t", "<fc=lightgreen><title> - <album> - <artist></fc>"]
+                1
+        , Run
+            $ Memory
+                [ "-t"
+                , "<fc=#ff79c6><fn=2>\xf035b</fn></fc>  <usedratio>% (<used>G)"
+                , "-d"
+                , "1"
+                , "--"
+                , "--scale"
+                , "1024"
+                ]
+                10
+        , Run
+            $ DiskU
+                [("/", "<fc=#ff79c6><fn=1>\xf1c0</fn></fc> <free>/<size>")]
+                ["-L", "50", "-H", "900", "-m", "1", "-p", "3"]
+                20
+        , Run
+            $ MultiCpu
+                [ "-t"
+                , "<fc=#ff79c6><fn=2>\xf4bc</fn></fc>   <total>%"
+                , "--Low"
+                , "10" -- units: %
+                , "--High"
+                , "90" -- units: %
+                , "--low"
+                , "white"
+                , "--normal"
+                , "yellow"
+                , "--high"
+                , "#ffcccb"
+                ]
+                20
+        , Run
+            $ MultiCoreTemp
+                [ "-t"
+                , "<avg>°C"
+                , "-L"
+                , "40"
+                , "-H"
+                , "85"
+                , "-l"
+                , "#21fdff"
+                , "-n"
+                , "white"
+                , "-h"
+                , "red"
+                , "--"
+                , "--mintemp"
+                , "20"
+                , "--maxtemp"
+                , "100"
+                ]
+                50
+        , Run $ Kbd []
+        , Run
+            $ CpuFreq
+                [ "-t"
+                , "<cpu0> GHz"
+                , "-L"
+                , "0"
+                , "-H"
+                , "5"
+                , "-l"
+                , "lightblue"
+                , "-n"
+                , "white"
+                , "-h"
+                , "orange"
+                ]
+                50
+        ]
+    , template =
+        "<hspace=10/>} %soundtrack% { %multicpu% %cpufreq% %multicoretemp%  %memory%  %disku% <hspace=10/>"
     , alignSep = "}{"
     }
 
--- src/xmobar.hs
+++ src/xmobar.hs
@@ -1,6 +1,5 @@
 import Plugins.Bluetooth
 import Plugins.Pomodoro
-import Plugins.Soundtrack
 import Theme.Font
 import Xmobar
 
@@ -26,27 +25,7 @@ config =
     , bgColor = "#5501c3"
     , commands =
         [ Run XMonadLog
-        , Run
-            $ Soundtrack
-                ["-t", "<fc=lightgreen><title> - <album> - <artist></fc>"]
-                1000
         , Run $ PomodoroSession Start
-        , Run
-            $ Memory
-                [ "-t"
-                , "<fc=#ff79c6><fn=2>\xf035b</fn></fc>  <usedratio>% (<used>G)"
-                , "-d"
-                , "1"
-                , "--"
-                , "--scale"
-                , "1024"
-                ]
-                10
-        , Run
-            $ DiskU
-                [("/", "<fc=#ff79c6><fn=1>\xf1c0</fn></fc> <free>/<size>")]
-                ["-L", "50", "-H", "900", "-m", "1", "-p", "3"]
-                20
         , Run
             $ Battery
                 [ "-t"
@@ -148,60 +127,6 @@ config =
                 , "True"
                 ]
                 10
-        , Run
-            $ MultiCpu
-                [ "-t"
-                , "<fc=#ff79c6><fn=2>\xf4bc</fn></fc>   <total>%"
-                , "--Low"
-                , "10" -- units: %
-                , "--High"
-                , "90" -- units: %
-                , "--low"
-                , "white"
-                , "--normal"
-                , "yellow"
-                , "--high"
-                , "#ffcccb"
-                ]
-                20
-        , Run
-            $ MultiCoreTemp
-                [ "-t"
-                , "<avg>°C"
-                , "-L"
-                , "40"
-                , "-H"
-                , "85"
-                , "-l"
-                , "#21fdff"
-                , "-n"
-                , "white"
-                , "-h"
-                , "red"
-                , "--"
-                , "--mintemp"
-                , "20"
-                , "--maxtemp"
-                , "100"
-                ]
-                50
-        , Run $ Kbd []
-        , Run
-            $ CpuFreq
-                [ "-t"
-                , "<cpu0> GHz"
-                , "-L"
-                , "0"
-                , "-H"
-                , "5"
-                , "-l"
-                , "lightblue"
-                , "-n"
-                , "white"
-                , "-h"
-                , "orange"
-                ]
-                50
         , Run $ Date "%a %_d %b %Y <fc=#ff79c6>%I:%M:%S %p</fc> %Z" "date" 10
         , Run
             $ Com
@@ -215,7 +140,7 @@ config =
             $ Uptime ["-t", "<fc=#ff79c6><days> <hours></fc>", "-S", "True"] 10
         ]
     , template =
-        "<hspace=10/>%pomodoro% %uptime%  %XMonadLog% }{ %dynnetwork% %bluetooth%  %default:Master% %soundtrack% %multicpu% %cpufreq% %multicoretemp%  %memory%  %disku%  %VIDP%  %date%<hspace=10/>"
+        "<hspace=10/>%pomodoro% %uptime%  %XMonadLog% }{ %dynnetwork% %bluetooth%  %default:Master%  %VIDP%  %date%<hspace=10/>"
     , alignSep = "}{"
     }
 
