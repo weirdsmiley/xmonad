f708486019d2baa5f630b240001bf7cee1a2d5f3
Manas <manas18244@iiitd.ac.in>
2025-02-01 23:52:13 +0530

    Fix layouts
    So I had mistakingly put mkToggle before setting per-workspace layouts
    which led to two issues:
    
    - newly added layouts not reloading at mod+q
    - toggle Full not working
    
    Fixed it. Also mkTabbed was putting two decorators so removed the old
    one and only kept the newly added top bar.
    
    Also added a new layout threeCol.

--- src/Layout.hs
+++ src/Layout.hs
@@ -13,6 +13,7 @@ import XMonad.Actions.TiledWindowDragging (dragWindow)
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout
 import XMonad.Layout.Accordion (Accordion(Accordion))
+import XMonad.Layout.BoringWindows (boringWindows)
 import XMonad.Layout.DraggingVisualizer (draggingVisualizer)
 import XMonad.Layout.LayoutModifier (ModifiedLayout)
 import XMonad.Layout.Magnifier
@@ -82,43 +83,42 @@ topBarTheme =
     }
 
 myLayout =
-  onWorkspace "1" (twoByThreeOnRight ||| twoByThreeOnLeft)
+  mkToggle (NOBORDERS ?? NBFULL ?? EOT) . avoidStruts . lessBorders Never
+    $ boringWindows
+    $ onWorkspace "1" (twoByThreeOnRight ||| twoByThreeOnLeft)
     $ onWorkspace "2" (multiColWithGaps ||| magnifiedMultiColWithGaps)
     $ onWorkspace "3" (multiColWithGaps ||| magnifiedMultiColWithGaps)
-    $ onWorkspace "4" (twoByThreeOnRight ||| full)
+    $ onWorkspace "4" threeCol
     $ onWorkspace "5" allLayouts
     $ onWorkspace "6" twoByThreeOnRight
     $ onWorkspace "7" allLayouts
     $ onWorkspace "8" allLayouts
     $ onWorkspace "9" twoByThreeOnRight
-    $ mkToggle (NOBORDERS ?? NBFULL ?? EOT)
-        . avoidStruts
-        . lessBorders OnlyScreenFloat
-    $ tiled
-        ||| twoByThreeOnRight
-        ||| twoByThreeOnLeft
-        ||| Mirror tiled
-        ||| tall
-        ||| full
-        ||| multiColWithGaps
+    $ allLayouts
   where
     multiColWithGaps =
-      rn "Columns" . addTopBar . addGaps $ multiCol [1] 1 0.01 (-0.5)
+      rn "Columns" . mkTabbed . addGaps . dragWindows
+        $ multiCol [1] 1 0.01 (-0.5)
     magnifiedMultiColWithGaps =
-      rn "MagnifiedColumns" . addTopBar . addGaps
+      rn "MagnifiedColumns" . mkTabbed . addGaps . dragWindows
         $ magnifiercz' 2.0
         $ multiCol [1] 1 0.01 (-0.5)
-    full = rn "Full" . addTopBar . addGaps $ Full
+    full = rn "Full" . mkTabbed . addGaps . dragWindows $ Full
     tall =
-      rn "Tall" . addTopBar . addGaps $ ResizableTall nmaster delta ratio []
-    threeCol = addTopBar . addGaps $ ThreeCol nmaster delta ratio
+      rn "Tall" . mkTabbed . addGaps . dragWindows
+        $ ResizableTall nmaster delta ratio []
+    threeCol =
+      rn "ThreeCol" . mkTabbed . addGaps . dragWindows
+        $ ResizableThreeColMid nmaster delta ratio []
     twoByThreeOnRight =
-      rn "2-by-3 (right)" . addTopBar . addGaps
+      rn "2-by-3 (right)" . mkTabbed . addGaps . dragWindows
         $ reflectHoriz
         $ Tall nmaster delta (2 / 3)
     twoByThreeOnLeft =
-      rn "2-by-3 (left)" . addTopBar . addGaps $ Tall nmaster delta (2 / 3)
-    tiled = rn "Tiled" . addTopBar . addGaps $ Tall nmaster delta ratio
+      rn "2-by-3 (left)" . mkTabbed . addGaps . dragWindows
+        $ Tall nmaster delta (2 / 3)
+    tiled =
+      rn "Tiled" . mkTabbed . addGaps . dragWindows $ Tall nmaster delta ratio
     nmaster = 1 -- Default number of windows in the master pane
     ratio = 1 / 2 -- Default proportion of screen occupied by master pane
     delta = 3 / 100 -- Percent of screen to increment by when resizing panes
@@ -126,12 +126,11 @@ myLayout =
          Integer -> l a -> ModifiedLayout XMonad.Layout.Spacing.Spacing l a
     mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
     rn n = renamed [Replace n]
-    mkTabbed layout =
-      addTabs shrinkText myTabConfig . subLayout [] (Simplest ||| Accordion)
-        $ layout
+    mkTabbed =
+      noFrillsDeco shrinkText topBarTheme
+        . subLayout [] (Simplest ||| Accordion)
     dragWindows layout = windowNavigation . draggingVisualizer $ layout
     addGaps = mySpacing myGaps
-    addTopBar = noFrillsDeco shrinkText topBarTheme
     allLayouts =
       multiColWithGaps
         ||| magnifiedMultiColWithGaps
@@ -140,3 +139,4 @@ myLayout =
         ||| twoByThreeOnLeft
         ||| twoByThreeOnRight
         ||| tiled
+        ||| threeCol
