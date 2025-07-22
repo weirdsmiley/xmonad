a92d527f69db3f5d70b4ebc9d3d97476e9fcb660
Manas <manas18244@iiitd.ac.in>
2025-07-22 23:40:09 +0530

    Enable fading for unfocused windows
    When things become cluttered fade it all out, butter.

--- src/Preferences.hs
+++ src/Preferences.hs
@@ -171,7 +171,7 @@ applyOnlyOnCurrentWS = True
 
 -- Amount by which to fade inactive windows on current workspace only.
 fadeAmount :: Rational
-fadeAmount = 1.0
+fadeAmount = 0.2
 
 -- Music controller tool
 myMusicCtrl = "playerctl"
