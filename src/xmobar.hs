0d7b43ea89f89db71cbdcab0cfd92a1097be90b0
Manas <manas18244@iiitd.ac.in>
2025-10-15 16:30:03 +0530

    Remove Tux.xpm

--- src/xmobar.hs
+++ src/xmobar.hs
@@ -225,7 +225,7 @@ config =
                 50
         ]
     , template =
-        "<hspace=0/><icon=Tux.xpm/> %XMonadLog% %pomodoro% }{ %dynnetwork%  %bluetooth%  %default:Master%  %multicpu% %cpufreq% %multicoretemp%  %memory%  %disku%  %VIDP%  %date%<hspace=10/>"
+        "<hspace=0/> %XMonadLog% %pomodoro% }{ %dynnetwork%  %bluetooth%  %default:Master%  %multicpu% %cpufreq% %multicoretemp%  %memory%  %disku%  %VIDP%  %date%<hspace=10/>"
     , alignSep = "}{"
     }
 
