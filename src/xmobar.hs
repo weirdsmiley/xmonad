d3d089f5f0ebc872b65144212654b5c9e3d388e3
Manas <manas18244@iiitd.ac.in>
2025-10-23 13:16:50 +0530

    Add hspace on consistency

--- src/xmobar.hs
+++ src/xmobar.hs
@@ -225,7 +225,7 @@ config =
                 50
         ]
     , template =
-        "<hspace=0/> %XMonadLog% %pomodoro% }{ %dynnetwork%  %bluetooth%  %default:Master%  %multicpu% %cpufreq% %multicoretemp%  %memory%  %disku%  %VIDP%  %date%<hspace=10/>"
+        "<hspace=10/> %XMonadLog% %pomodoro% }{ %dynnetwork%  %bluetooth%  %default:Master%  %multicpu% %cpufreq% %multicoretemp%  %memory%  %disku%  %VIDP%  %date%<hspace=10/>"
     , alignSep = "}{"
     }
 
