bfb874f0708c4ca7f8f2e7e813de7d1d9408a9a4
Manas <manas18244@iiitd.ac.in>
2025-10-23 17:26:58 +0530

    Add a release branch with a fresh setup
    My setup contains startup apps, plugins, etc. which hinder in a fresh
    installation initial boot. This branch sleeks things to have a better
    user experience for first time installers.
    
    Signed-off-by: Manas <manas18244@iiitd.ac.in>

--- src/xmobar.hs
+++ src/xmobar.hs
@@ -225,7 +225,7 @@ config =
                 50
         ]
     , template =
-        "<hspace=10/> %XMonadLog% %pomodoro% }{ %dynnetwork%  %bluetooth%  %default:Master%  %multicpu% %cpufreq% %multicoretemp%  %memory%  %disku%  %VIDP%  %date%<hspace=10/>"
+        "<hspace=10/> %XMonadLog% }{ %dynnetwork%  %default:Master%  %multicpu% %cpufreq%   %memory%  %disku%  %VIDP%  %date%<hspace=10/>"
     , alignSep = "}{"
     }
 
